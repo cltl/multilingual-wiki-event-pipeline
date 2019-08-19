8df9388a93921ef460800dc46cbfa81128d685e0
Filip Ilievski <filip.dbrsk@gmail.com>
2019-08-19 14:49:40 +0200

    cleaning up

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-#pilot=False
-pilot=True
-#incident_types=['election']
-incident_types=['murder']
+pilot=False
+#pilot=True
+incident_types=['election']
+#incident_types=['murder']
 languages_list=[['nl', 'it', 'en']]
