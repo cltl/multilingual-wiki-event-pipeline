86172de492cec84a5aae1d9fe89fb2d75b768691
Filip Ilievski <filip.dbrsk@gmail.com>
2019-11-21 17:32:39 +0100

    config for paper

--- config.py
+++ config.py
@@ -3,7 +3,8 @@
 #incident_types=['murder']
 #incident_types=['conflagration', 'earthquake', 'murder']
 #incident_types=['conflagration', 'earthquake', 'battle', 'murder', 'film festival', 'space flight', 'art exhibition', 'football match', 'convention', 'wrestling event', 'terrorist attack', 'tennis tournament']
-incident_types=['Q132821', 'Q168983']
+incident_types=['Q132821', 'Q168983', 'Q464980', 'Q132241', 'Q4504495', 'Q3001412', 'Q220505', 'Q40244', 'Q645883', 'Q62391930']
+#incident_types=['Q40231']
 languages_list=['nl', 'it', 'en']
 
 # Pilot extraction flags
@@ -12,8 +13,8 @@ must_have_english=True #True
 one_page_per_language=False # True
 
 # Analyze pilot data only, or the full data
-pilot=True
-#False
+pilot=True #False
+#pilot=False
 
 qid2fn={'Q40231': 'change_of_leadership', 'Q132821': 'killing'} #, 'tennis tournament': 'tennis tournament'}
 
