584f395bcc76f53613f899b15522b3c7a4f60841
MartenPostma <martenp@gmail.com>
2020-04-20 10:10:03 +0200

    updated pilot utils

--- pilot_utils.py
+++ pilot_utils.py
@@ -1,116 +1,124 @@
-import time
-import pickle
 import json
-import re
 import os
+import re
+import time
 import urllib.parse
+
+import spacy_to_naf
 from lxml import etree
 
-import xml_utils
-import classes
-import config
-import utils
 import native_api_utils as api
-import spacy_to_naf
+import utils
+import xml_utils
 
-eventtype2json=config.qid2fn
-#, 'tennis tournament': 'tennis tournament'}
+eventtype2json = {}
 
-target_languages=config.languages_list
+
+# , 'tennis tournament': 'tennis tournament'}
 
 def remove_incidents_with_missing_FEs(incidents, event_type):
-    new_incidents=[]
+    new_incidents = []
 
     print('EType', event_type, eventtype2json.keys())
     if event_type in eventtype2json.keys():
-        jsonfilename='wdt_fn_mappings/%s.json' % eventtype2json[event_type]
+        jsonfilename = 'wdt_fn_mappings/%s.json' % eventtype2json[event_type]
     else:
-        jsonfilename='wdt_fn_mappings/any.json'
+        jsonfilename = 'wdt_fn_mappings/any.json'
 
     with open(jsonfilename, 'rb') as f:
-        wdt_fn_mappings_COL=json.load(f)
+        wdt_fn_mappings_COL = json.load(f)
 
-    all_frame_elements=set(wdt_fn_mappings_COL.keys())
+    all_frame_elements = set(wdt_fn_mappings_COL.keys())
 
     for incident in incidents:
-        extra_info_keys=set(incident.extra_info.keys())
-        if extra_info_keys==all_frame_elements:
+        extra_info_keys = set(incident.extra_info.keys())
+        if extra_info_keys == all_frame_elements:
             new_incidents.append(incident)
     return new_incidents
 
 
 def check_ref_text(rt, min_chars=100, max_chars=10000):
-    num_chars=len(rt.content)
-    if num_chars<min_chars or num_chars>max_chars:
+    num_chars = len(rt.content)
+    if num_chars < min_chars or num_chars > max_chars:
         return False
     if re.match(r'.*[1-2]([0-9]){3}-[1-2]([0-9]){3}.*$', rt.name):
         return False
     return True
 
-def skip_this_incident(ref_texts, rt_langs, must_have_all_languages=True, must_have_english=True, one_page_per_language=True):
+
+def skip_this_incident(ref_texts,
+                       target_languages,
+                       rt_langs,
+                       must_have_all_languages=True,
+                       must_have_english=True,
+                       one_page_per_language=True):
     """ Perform language checks depending on flags set in the config file."""
-    skip_incident=False
+    skip_incident = False
     if must_have_all_languages:
         for target_lang in target_languages:
             if target_lang not in rt_langs:
-                skip_incident=True
+                skip_incident = True
                 break
-    if must_have_english and 'en' not in rt_langs: 
-        skip_incident=True
-    elif one_page_per_language and len(ref_texts)!=len(rt_langs):
-        skip_incident=True
+    if must_have_english and 'en' not in rt_langs:
+        skip_incident = True
+    elif one_page_per_language and len(ref_texts) != len(rt_langs):
+        skip_incident = True
     return skip_incident
 
+
 def create_pilot_data(data,
+                      target_languages,
                       must_have_all_languages,
                       must_have_english,
                       one_page_per_language):
-    pilot_incidents=set()
+    pilot_incidents = set()
 
-    cached={}
+    cached = {}
 
-    data.incidents=remove_incidents_with_missing_FEs(data.incidents, data.incident_type)
+    data.incidents = remove_incidents_with_missing_FEs(data.incidents, data.incident_type)
     for incident in data.incidents:
-        langs=set()
-        incident.reference_texts=utils.deduplicate_ref_texts(incident.reference_texts)
-        new_ref_texts=[]
+        langs = set()
+        incident.reference_texts = utils.deduplicate_ref_texts(incident.reference_texts)
+        new_ref_texts = []
         for ref_text in incident.reference_texts:
-            ref_text.content=ref_text.content.split('==')[0].strip() # first section
+            ref_text.content = ref_text.content.split('==')[0].strip()  # first section
             if check_ref_text(ref_text, max_chars=50000):
                 langs.add(ref_text.language)
                 new_ref_texts.append(ref_text)
-        incident.reference_texts=new_ref_texts
+        incident.reference_texts = new_ref_texts
 
         if skip_this_incident(new_ref_texts,
-                             langs,
-                             must_have_all_languages,
-                             must_have_english,
-                             one_page_per_language):
+                              target_languages,
+                              langs,
+                              must_have_all_languages,
+                              must_have_english,
+                              one_page_per_language):
             continue
-            
+
         for ref_text in incident.reference_texts:
             if not ref_text.uri:
-                ref_text.uri=api.get_uri_from_title(ref_text.name, ref_text.language)
+                ref_text.uri = api.get_uri_from_title(ref_text.name, ref_text.language)
         pilot_incidents.add(incident)
         for p, v_set in incident.extra_info.items():
-            new_v_set=set()
+            new_v_set = set()
             for v in v_set:
                 if '|' not in v:
-                    label=''
-                    q_id=v.split('/')[-1]
+                    label = ''
+                    q_id = v.split('/')[-1]
                     if q_id in cached.keys():
-                        label=cached[q_id]
+                        label = cached[q_id]
                     elif v.startswith('http'):
-                        label=utils.obtain_label(q_id)
+                        label = utils.obtain_label(q_id)
                         time.sleep(1)
-                        cached[q_id]=label
-                    v+=' | ' + label
+                        cached[q_id] = label
+                    v += ' | ' + label
                     new_v_set.add(v)
                 else:
                     new_v_set.add(v)
-            incident.extra_info[p]=new_v_set
+            incident.extra_info[p] = new_v_set
     print('Num of pilot incidents', len(pilot_incidents))
     return pilot_incidents
+
 
 def load_annotations(annotations, prefix):
     """
@@ -134,6 +142,7 @@ def load_annotations(annotations, prefix):
 
     return start_end2info
 
+
 def add_hyperlinks(naf, annotations, prefix, language, dct, wiki_langlinks={}, verbose=0):
     """
     :param lxml.etree._Element naf: the root element of the XML file    :param wiki_page:
@@ -148,11 +157,11 @@ def add_hyperlinks(naf, annotations, prefix, language, dct, wiki_langlinks={}, v
     start_end2info = load_annotations(annotations,
                                       prefix=prefix)
 
-    start_offset2token = {int(w_el.get('offset')) : w_el.text
+    start_offset2token = {int(w_el.get('offset')): w_el.text
                           for w_el in naf.xpath('text/wf')}
 
     next_id = 1
-    naf=naf.getroot()
+    naf = naf.getroot()
     entities_layer = etree.SubElement(naf, "entities")
 
     naf_header = naf.find('nafHeader')
@@ -163,7 +172,7 @@ def add_hyperlinks(naf, annotations, prefix, language, dct, wiki_langlinks={}, v
     lp.set("beginTimestamp", the_time)
     lp.set('endTimestamp', the_time)
     lp.set('name', 'Wikipedia')
-    lp.set('version', '2019-07-20') # TODO: change this if we move to other version of Wikipedia
+    lp.set('version', '2019-07-20')  # TODO: change this if we move to other version of Wikipedia
 
     for (start, end), (sf, uri) in start_end2info.items():
 
@@ -183,30 +192,32 @@ def add_hyperlinks(naf, annotations, prefix, language, dct, wiki_langlinks={}, v
         t_ids = xml_utils.get_range_of_tids(start_tid,
                                             end_tid)
 
-        ext_refs = [{'resource' : 'Wikipedia', 'reference': uri}]
+        ext_refs = [{'resource': 'Wikipedia', 'reference': uri}]
         if wiki_langlinks:
             for lang, uri in wiki_langlinks[language][uri].items():
-                ext_refs.append({'resource' : 'Wikipedia', 'reference': uri})
+                ext_refs.append({'resource': 'Wikipedia', 'reference': uri})
 
-        entity_data=spacy_to_naf.EntityElement(
-                                             eid='e%d' % next_id,
-                                             entity_type='UNK',
-                                             text=sf,
-                                             targets=t_ids,
-                                             ext_refs=ext_refs)
+        entity_data = spacy_to_naf.EntityElement(
+            eid='e%d' % next_id,
+            entity_type='UNK',
+            text=sf,
+            targets=t_ids,
+            ext_refs=ext_refs)
         next_id += 1
 
         spacy_to_naf.add_entity_element(entities_layer, entity_data, add_comments=True)
 
+
 def text_to_naf(wiki_title,
+                target_languages,
                 text,
                 wiki_uri,
                 annotations,
                 prefix,
-		        language,
-		        nlp, 
-		        dct,
-		        output_folder=None,
+                language,
+                nlp,
+                dct,
+                output_folder=None,
                 wiki_langlinks={},
                 verbose=0):
     assert language in target_languages, f'{language} not part of supported languages: {" ".join(target_languages)}'
@@ -225,8 +236,8 @@ def text_to_naf(wiki_title,
 
     # add hyperlinks as entity elements
     add_hyperlinks(naf,
-	               annotations,
-	               prefix,
+                   annotations,
+                   prefix,
                    language,
                    dct,
                    wiki_langlinks=wiki_langlinks)
