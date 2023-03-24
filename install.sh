797f2e01f8134a3aa55b00f07fedc1e28786ade6
Pia Sommerauer <piasommerauer@dhcp-083-158.th.vu.nl>
2023-03-24 13:08:30 +0100

    adding updated install.sh

--- install.sh
+++ install.sh
@@ -10,10 +10,9 @@ cd resources
 
 wget http://kyoto.let.vu.nl/~postma/dfn/mwep/merged_indices.p
 
-git clone https://github.com/cltl/SpaCy-to-NAF
-cp SpaCy-to-NAF/spacy_to_naf.py ../
+# Pia: needs update - added pip install to requirements
+# git clone https://github.com/cltl/SpaCy-to-NAF
+# cp SpaCy-to-NAF/spacy_to_naf.py ../
 
 wget http://kyoto.let.vu.nl/~postma/dfn/mwep/Wikipedia_Reader.zip
 unzip Wikipedia_Reader.zip
-
-
