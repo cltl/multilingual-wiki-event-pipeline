121107bfa7969f3e112f8795747b46a2b0f48d98
piekvossen <piekvossen@gmail.com>
2023-05-23 17:32:24 +0200

    - new wikidata query api for participant events

--- utils.py
+++ utils.py
@@ -143,6 +143,120 @@ def construct_and_run_query(type_qid,
     return results_by_id
 
 
+#@TODO:
+# - create a query to look for a participant in an event of a certain type
+# - create a fake instance ID by combining the q-code of the person with the q-code of the event-type
+#defaultView:ImageGrid
+# SELECT ?person ?prop ?disease
+# WHERE
+# {
+# ?person ?prop ?disease .
+# ?person wdt:P31 wd:Q5 .
+#       ?disease wdt:P31 wd:Q18123741  #infectious disease
+# SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en" }
+# }
+
+# {
+#   "head" : {
+#     "vars" : [ "person", "prop", "disease" ]
+#   },
+#   "results" : {
+#     "bindings" : [ {
+#       "disease" : {
+#         "type" : "uri",
+#         "value" : "http://www.wikidata.org/entity/Q2840"
+#       },
+#       "person" : {
+#         "type" : "uri",
+#         "value" : "http://www.wikidata.org/entity/Q720370"
+#       },
+#       "prop" : {
+#         "type" : "uri",
+#         "value" : "http://www.wikidata.org/prop/direct/P509"
+#       }
+#     }, {
+#       "disease" : {
+#         "type" : "uri",
+#         "value" : "http://www.wikidata.org/entity/Q2840"
+#       },
+#       "person" : {
+#         "type" : "uri",
+#         "value" : "http://www.wikidata.org/entity/Q724677"
+#       },
+#       "prop" : {
+#         "type" : "uri",
+#         "value" : "http://www.wikidata.org/prop/direct/P509"
+#       }
+#     }
+#   }
+# }
+def construct_and_run_participant_query(type_qid,
+                            event_type_matching,
+                            languages,
+                            more_props,
+                            limit):
+    """
+    Construct a wikidata query to obtain all events of a specific type with their structured data, then run this query.
+    """
+
+    lang2var={}
+    for l in languages:
+        var='?label_%s' % l
+        lang2var[l]=var
+    return_langs=' '.join(lang2var.values())
+
+    optional_clauses_str=""
+    for l, var in lang2var.items():
+        clause=f"""OPTIONAL {{ \n\t?incident rdfs:label {var}.\n\tFILTER ( LANGMATCHES ( LANG ( {var} ), \"{l}\" )) }}\n\t"""
+        optional_clauses_str+=clause
+
+    opt_vars=[]
+    opt_var_labels=[]
+    optional_more_info=""
+    for fn_role, wdt_prop_paths in more_props.items():
+        for a_path in wdt_prop_paths:
+            var='?' + a_path.replace('wdt:', '').replace('/', '_')       # fn_role.split('@')[-1]
+            if var not in opt_vars:
+                label_var=f"{var}Label"
+                clause=f"""OPTIONAL {{ \n\t?incident {a_path} {var} }}\n\t"""
+                optional_more_info+=clause
+                opt_vars.append(var)
+                if type_qid not in {"Q40231"}:
+                    opt_var_labels.append(var + 'Label')
+
+    #### Only works for persons
+    main_part = f'?person ?prop ?disease .\n?person wdt:P31 wd:Q5 .\n ?disease wdt:P31 type_qid'
+    if event_type_matching == 'direct_match':
+        main_part = f'?incident wdt:P31 wd:{type_qid} .\nBIND(wd:{type_qid} as ?direct_type) .'
+    elif event_type_matching == 'subsumed_by':
+        main_part = f'?incident wdt:P31*/wdt:P279* wd:{type_qid} ;\nwdt:P31 ?direct_type .'
+
+    query = """
+    SELECT DISTINCT ?direct_type ?incident ?incidentLabel %s %s %s WHERE {
+      SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+      %s
+      %s
+      %s
+    } limit %d
+    """ % (return_langs,
+           ' '.join(opt_vars),
+           ' '.join(opt_var_labels),
+           main_part,
+           optional_clauses_str,
+           optional_more_info,
+           limit)
+
+    print('QUERY:\n', query)
+
+    response=get_results_with_retry(wdt_sparql_url, query)
+
+    results=response['results']['bindings']
+
+    results_by_id=index_results_by_id(results, lang2var, more_props)
+
+    return results_by_id
+
+
 def index_results_by_id(raw_results, lang2var, extra_info):
     """
     Aggregate/index the SPARQL results by incident ID.
@@ -174,6 +288,76 @@ def index_results_by_id(raw_results, lang2var, extra_info):
         for predicate, wdt_prop_paths in extra_info.items():
             for a_path in wdt_prop_paths:
                 var=a_path.replace('wdt:', '').replace('/', '_')       # fn_role.split('@')[-1] 
+                if var in entry.keys() and entry[var]['value']:
+                    if var + 'Label' in entry.keys() and entry[var + 'Label']['value']:
+                        complex_value='%s | %s' % (entry[var]['value'], entry[var + 'Label']['value'])
+                    else:
+                        complex_value=entry[var]['value']
+                    current_result['extra_info'][predicate].add(complex_value)
+        indexed_results[wdt_id]=current_result
+    return indexed_results
+
+#@TODO: adapt this funciton to handle the results for the participant query
+#   "results" : {
+#     "bindings" : [ {
+#       "disease" : {
+#         "type" : "uri",
+#         "value" : "http://www.wikidata.org/entity/Q2840"
+#       },
+#       "person" : {
+#         "type" : "uri",
+#         "value" : "http://www.wikidata.org/entity/Q720370"
+#       },
+#       "prop" : {
+#         "type" : "uri",
+#         "value" : "http://www.wikidata.org/prop/direct/P509"
+#       }
+#     }, {
+#       "disease" : {
+#         "type" : "uri",
+#         "value" : "http://www.wikidata.org/entity/Q2840"
+#       },
+#       "person" : {
+#         "type" : "uri",
+#         "value" : "http://www.wikidata.org/entity/Q724677"
+#       },
+#       "prop" : {
+#         "type" : "uri",
+#         "value" : "http://www.wikidata.org/prop/direct/P509"
+#       }
+#     }
+#   }
+def index_results_by_participant_id(raw_results, lang2var, extra_info):
+    """
+    Aggregate/index the SPARQL results by incident ID.
+    """
+    indexed_results=defaultdict(dict)
+    for entry in raw_results:
+        wdt_id=entry['incident']['value']
+        current_result=indexed_results[wdt_id]
+        if not len(current_result.keys()):
+            current_result=defaultdict(dict)
+
+        if 'references' not in current_result:
+            current_result['references']=defaultdict(str)
+        name=entry['incidentLabel']['value']
+        #current_result['type_id']=entry['type_id']['value']
+
+        if 'direct_types' not in current_result.keys():
+            current_result['direct_types']=set()
+        current_result['direct_types'].add(entry['direct_type']['value'])
+
+        for l, var in lang2var.items():
+            label_in_lang=var.strip('?')
+            if label_in_lang in entry.keys():
+                name_in_lang=entry[label_in_lang]['value']
+                current_result['references'][l]=name_in_lang
+
+        if 'extra_info' not in current_result.keys():
+            current_result['extra_info']=defaultdict(set)
+        for predicate, wdt_prop_paths in extra_info.items():
+            for a_path in wdt_prop_paths:
+                var=a_path.replace('wdt:', '').replace('/', '_')       # fn_role.split('@')[-1]
                 if var in entry.keys() and entry[var]['value']:
                     if var + 'Label' in entry.keys() and entry[var + 'Label']['value']:
                         complex_value='%s | %s' % (entry[var]['value'], entry[var + 'Label']['value'])
